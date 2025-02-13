3a91941d6e505d30e228749806a9f91e9b9f6f3c
GaelFdzz <gaelandrade272@gmail.com>
2025-02-12 22:44:08 -0500

    init

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+
+const images = {
+  1: require('./images/1.jpg'),
+  2: require('./images/2.jpg'),
+  3: require('./images/3.jpg'),
+  4: require('./images/4.jpg'),
+  5: require('./images/5.jpg'),
+};
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [image, setImage] = useState(null);
+
+  const imagenRandom = () => {
+    const numeroRandom = Math.floor(Math.random() * 5) + 1;
+    return images[numeroRandom];
+  };
+
+  const sumarContador = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+
+    if (newCount % 10 === 0) {
+      setImage(imagenRandom());
+    }
+
+    if (newCount == 51) {
+      setCount(0);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {image && (
+        <View style={styles.imageContainer}>
+          <Image source={image} style={styles.image} />
+        </View>
+      )}
+
+      <Text style={styles.counterText}>{count}</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={sumarContador}>
+          <Text>Presiona aquí</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,5 +56,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    position: 'absolute',  // Fija la imagen en la parte superior
+    top: 50,               // Ajusta la posición desde la parte superior
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',  // Fija el botón en su lugar
+    bottom: 100,           // Ajusta la distancia desde la parte inferior
+  },
+  counterText: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
